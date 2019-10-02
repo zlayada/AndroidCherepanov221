e34cd66fa5c7806b93eeea1cae7a1462632c8d99
Roman Cherepanov <r.e.cherepanov@gmail.com>
2019-10-02 21:22:31 +0300

    Initial commit

---
+++ app/src/main/java/com/netology/androidcherepanov221/MainActivity.java
@@ -0,0 +1,60 @@
+package com.netology.androidcherepanov221;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputNote;
+    private Button mBtnSaveNote;
+
+    private SharedPreferences myNoteSharedPref;
+    private static String NOTE_TEXT = "note_text";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        getDateFromSharedPref();
+    }
+
+    //Инициализируем элементы экрана
+    private void initViews() {
+        mInputNote = findViewById(R.id.inputNote);
+        mBtnSaveNote = findViewById(R.id.btnSaveNote);
+
+        myNoteSharedPref = getSharedPreferences("MyNote", MODE_PRIVATE);
+        mBtnSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // Обрабатываем кнопку "ОК" Выводим тост о сохранении данных
+        mBtnSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor myEditor = myNoteSharedPref.edit();
+                String noteTxt = mInputNote.getText().toString();
+                myEditor.putString(NOTE_TEXT, noteTxt);
+                myEditor.apply();
+                Toast.makeText(MainActivity.this, "Данные сохранены", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void getDateFromSharedPref(){
+        String noteTxt = myNoteSharedPref.getString(NOTE_TEXT, "");
+        mInputNote.setText(noteTxt);
+    }
+
+
+}
